efe810930fc8fc9a2e32c23a4bb6fdb0ab680b36
rodrigovvo <rodrigovvo@gmail.com>
2022-04-02 17:40:22 -0300

    #2 - Novos testes

--- core/api/urls.py
+++ core/api/urls.py
@@ -7,10 +7,10 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-router.register('raffles', views.RaffleViewSet)
-router.register('campaigns', views.CampaignViewSet)
-router.register('tickets', views.TicketViewSet)
-router.register('prizes', views.PrizeViewSet)
+router.register(r'raffles', views.RaffleViewSet)
+router.register(r'campaigns', views.CampaignViewSet)
+router.register(r'tickets', views.TicketViewSet)
+router.register(r'prizes', views.PrizeViewSet)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
